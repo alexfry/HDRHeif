fabc1d0b4371061940d29df61b864245b60bca28
Alex Fry <alexfry@gmail.com>
2020-04-08 13:13:16 +1000

    rejigged script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HDRHeif
- HDR Heif image creation
+ HDR Heif image creation investigation.
+
+ Basic script usage:
+ `python quickHEIF_Wrap_v001.py sourceImages/ramp_Rec2020-PQ.png HeicExamples`
--- quickHEIF_Wrap_v001.py
+++ quickHEIF_Wrap_v001.py
@@ -4,6 +4,11 @@ import sys
 import subprocess
 import os
 
+tempDir = 'tempDir'
+outPath = ''
+if len(sys.argv) > 2:
+	outPath = sys.argv[2]
+
 inputFileMainName = sys.argv[1].split('/').pop(-1).split('.').pop(0)
 
 encodeCommand = '''ffmpeg -y -i INPUTFILE  -pix_fmt yuv420p -f hevc -x265-params "lossless=1:keyint=60:bframes=3:vbv-bufsize=75000:vbv-maxrate=75000:colorprim=bt2020:transfer=smpte2084:colormatrix=bt2020nc:master-display='G(8500,39850)B(6550,2300)R(35400,14600)WP(15635,16450)L(10000000,1)'" OUTPUTFILE'''
@@ -12,13 +17,18 @@ encodeCommand = '''ffmpeg -y -i INPUTFILE  -pix_fmt yuv420p -f hevc -x265-params
 encodeThumbCommand = '''ffmpeg -y -i INPUTFILE -vf scale=320:240 -crf 28 -preset slower    -pix_fmt yuv420p -f hevc -x265-params "keyint=60:bframes=3:vbv-bufsize=75000:vbv-maxrate=75000:colorprim=bt2020:transfer=smpte2084:colormatrix=bt2020nc:master-display='G(8500,39850)B(6550,2300)R(35400,14600)WP(15635,16450)L(10000000,1)'" OUTPUTFILE'''
 
 OutputName = inputFileMainName+'.265'
+OutputPath = os.path.join(tempDir,OutputName)
+print OutputPath
 #OutputVideoName = inputFileMainName+'.mp4'
 OutputThumbName = inputFileMainName+'_thumb.265'
-OutputHeicName = inputFileMainName+'.heic'
+OutputThumbPath = os.path.join(tempDir,OutputThumbName)
 
-finalEncodeCommand = encodeCommand.replace('INPUTFILE',sys.argv[1]).replace('OUTPUTFILE',OutputName)
+OutputHeicName = inputFileMainName+'.heic'
+OutputHeicPath = os.path.join(outPath,OutputHeicName)
+
+finalEncodeCommand = encodeCommand.replace('INPUTFILE',sys.argv[1]).replace('OUTPUTFILE',OutputPath)
 #finalVideoEncodeCommand = encodeVideoCommand.replace('INPUTFILE',sys.argv[1]).replace('OUTPUTFILE',OutputVideoName)
-finalThumbEncodeCommand = encodeThumbCommand.replace('INPUTFILE',sys.argv[1]).replace('OUTPUTFILE',OutputThumbName)
+finalThumbEncodeCommand = encodeThumbCommand.replace('INPUTFILE',sys.argv[1]).replace('OUTPUTFILE',OutputThumbPath)
 
 
 print finalEncodeCommand
@@ -56,16 +66,19 @@ configContents = '''{
 	}]
 }'''
 
-finalConfigContents = configContents.replace('HEICOUTPUT',OutputHeicName).replace('OUTPUTFILE',OutputName).replace('OUTPUTTHUMBFILE',OutputThumbName)
+
+
+finalConfigContents = configContents.replace('HEICOUTPUT',OutputHeicPath).replace('OUTPUTFILE',OutputPath).replace('OUTPUTTHUMBFILE',OutputThumbPath)
 
 configName = inputFileMainName+'.json'
+configPath = os.path.join(tempDir,configName)
 
-configFile = open(configName,"w+")
+configFile = open(configPath,"w+")
 configFile.write(finalConfigContents)
 configFile.close()
 
 
-os.system('~/GitHub/heif/Bins/writerapp ' +configName)
+os.system('~/GitHub/heif/Bins/writerapp ' +configPath)
 
 print 'done'
 
